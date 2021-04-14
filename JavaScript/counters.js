2eaa3cbacc00ffbb9fe4d2c047a2c9de5d8846b6
Bojan <bojanmitrovic@hotmail.rs>
2021-04-14 12:59:09 +0200

    added observer for counters, reorganized js and css folder

---
+++ JavaScript/counters.js
@@ -0,0 +1,52 @@
+const counters = document.querySelectorAll('.counter');
+const counterWrap = document.querySelector('.counters');
+// const counterWrap = document.querySelectorAll('.counter-wrap')
+const speed = 50;
+
+// for each counter number 
+const risingNumbers = () => {
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+
+            // counting spped rate
+            const inc = target / speed;
+
+
+            console.log(inc);
+            console.log(count);
+
+            if (count < target) {
+
+                counter.innerText = Math.ceil(count + inc);
+
+                setTimeout(updateCount, 30);
+            } else {
+                counter.innerText = target;
+            }
+
+        };
+        updateCount();
+    })
+}
+
+
+// start counting when scroll down to the page
+const counterOptions = {
+    threshold: 1,
+    rootMargin: '0px 0px 60px 0px'
+};
+const countingUp = new IntersectionObserver(function(entries, countingUp) {
+
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            risingNumbers();
+        }
+    });
+
+}, counterOptions);
+
+countingUp.observe(counterWrap);
